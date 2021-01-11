cdb332249cd527ab7aa0f6558505bd6aacf056fe
olatzinfante <47973617+olatzinfante@users.noreply.github.com>
2021-01-11 19:52:52 +0100

    Cambio en EasyBookingRemoteFacade

--- EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/remote/EasyBookingRemoteFacade.java
+++ EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/remote/EasyBookingRemoteFacade.java
@@ -3,10 +3,7 @@ package es.deusto.ingenieria.sd.easyB.server.remote;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-
-
-
-
+// <<Singletone>> Implemeta la interface IEasyBookingRemoteFacade
 public class EasyBookingRemoteFacade extends UnicastRemoteObject implements IEasyBookingRemoteFacade{
 
 	private static final long serialVersionUID = 1L;
@@ -16,15 +13,15 @@ public class EasyBookingRemoteFacade extends UnicastRemoteObject implements IEas
 		super();		
 	}
 	
+	// Lazy Initialization
 	public static IEasyBookingRemoteFacade getInstance() {
 		if (instance == null) {
 			try {
 				instance = new EasyBookingRemoteFacade();
 			} catch (Exception ex) {
-				System.err.println("# Error creating RemoteFacade: " + ex);
+				System.err.println("# Error creating EasyBookingRemoteFacade: " + ex);
 			}
-		}
-		
+		}	
 		return instance;
 	}
 
