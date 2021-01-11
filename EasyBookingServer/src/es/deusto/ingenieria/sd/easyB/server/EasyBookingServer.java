ecfe578fc25ce2e16782e5606cdf2ee4dfbf78be
olatzinfante <47973617+olatzinfante@users.noreply.github.com>
2021-01-11 19:48:52 +0100

    EasyBookingServer: Hecho EasyBookingRemoteFacade: Empezado

--- EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/EasyBookingServer.java
+++ EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/EasyBookingServer.java
@@ -1,5 +1,25 @@
 package es.deusto.ingenieria.sd.easyB.server;
 
-public class EasyBookingServer {
+import java.rmi.Naming;
 
+import es.deusto.ingenieria.sd.easyB.server.remote.EasyBookingRemoteFacade;
+import es.deusto.ingenieria.sd.easyB.server.remote.IEasyBookingRemoteFacade;
+
+public class EasyBookingServer {
+	public static void main(String[] args) {	
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+
+		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];		
+		
+		try {
+			IEasyBookingRemoteFacade remoteFacade = EasyBookingRemoteFacade.getInstance();			
+			Naming.rebind(name, remoteFacade);
+			System.out.println(" * EasyBooking server '" + name + "' started");
+		} catch (Exception ex) {
+			System.err.println(" # EasyBooking Server Exception: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
 }
--- EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/remote/EasyBookingRemoteFacade.java
+++ EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/remote/EasyBookingRemoteFacade.java
@@ -1,5 +1,31 @@
 package es.deusto.ingenieria.sd.easyB.server.remote;
 
-public class EasyBookingRemoteFacade {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+
+
+
+
+public class EasyBookingRemoteFacade extends UnicastRemoteObject implements IEasyBookingRemoteFacade{
+
+	private static final long serialVersionUID = 1L;
+	private static EasyBookingRemoteFacade instance;
+	
+	private EasyBookingRemoteFacade() throws RemoteException {
+		super();		
+	}
+	
+	public static IEasyBookingRemoteFacade getInstance() {
+		if (instance == null) {
+			try {
+				instance = new EasyBookingRemoteFacade();
+			} catch (Exception ex) {
+				System.err.println("# Error creating RemoteFacade: " + ex);
+			}
+		}
+		
+		return instance;
+	}
 
 }
