cc81cb99511815710800ba5ce61048686031bcc2
Ainaraast <ainara.astondoa@opendeusto.es>
2021-01-17 14:56:47 +0100

    Cambios en Getaway Servicio de autorizacion

--- EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/gateway/GatewayGoogle.java
+++ EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/gateway/GatewayGoogle.java
@@ -1,17 +1,20 @@
 package es.deusto.ingenieria.sd.easyB.server.gateway;
 
+import es.deusto.ingenieria.sd.easyB.server.data.Aeropuerto;
+import es.deusto.ingenieria.sd.easyB.server.data.SistemaPago;
 import es.deusto.ingenieria.sd.easyB.server.data.Usuario;
 
 public class GatewayGoogle implements IGatewayAutorizacion{
 
+
 	@Override
-	public Usuario registrarUsuario(String nombre) {
+	public Usuario login(String email, String password) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Usuario login(String email, String password) {
+	public Usuario registrarUsuario(String email, String password, SistemaPago tipoPago, Aeropuerto aeroPref) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/gateway/IGatewayAutorizacion.java
+++ EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/gateway/IGatewayAutorizacion.java
@@ -1,10 +1,12 @@
 package es.deusto.ingenieria.sd.easyB.server.gateway;
 
+import es.deusto.ingenieria.sd.easyB.server.data.Aeropuerto;
+import es.deusto.ingenieria.sd.easyB.server.data.SistemaPago;
 import es.deusto.ingenieria.sd.easyB.server.data.Usuario;
 
 public interface IGatewayAutorizacion {
 	//un metodo por cada funcionalidad que nos ofrezca el servicio externo
-	public Usuario registrarUsuario(String nombre);
+	public Usuario registrarUsuario(String email, String password, SistemaPago tipoPago, Aeropuerto aeroPref);
 	public Usuario login(String email, String password);
 
 }
