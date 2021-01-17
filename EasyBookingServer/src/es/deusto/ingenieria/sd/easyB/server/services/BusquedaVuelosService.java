340d181b712b4bb263179ef85a4a0e8312902a96
leire <leire@DESKTOP-3CIGEBQ>
2021-01-17 13:41:57 +0100

    Correcion BusquedaAppService

--- EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/services/BusquedaVuelosService.java
+++ EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/services/BusquedaVuelosService.java
@@ -1,11 +1,8 @@
 package es.deusto.ingenieria.sd.easyB.server.services;
 
 import java.util.List;
-
-import es.deusto.ingenieria.sd.easyB.server.dao.AerolineaDAO;
-import es.deusto.ingenieria.sd.easyB.server.dao.AeropuertoDAO;
 import es.deusto.ingenieria.sd.easyB.server.dao.DBManager;
-import es.deusto.ingenieria.sd.easyB.server.dao.VueloDAO;
+import es.deusto.ingenieria.sd.easyB.server.data.Aerolinea;
 import es.deusto.ingenieria.sd.easyB.server.data.dto.AerolineaAssembler;
 import es.deusto.ingenieria.sd.easyB.server.data.dto.AerolineaDTO;
 import es.deusto.ingenieria.sd.easyB.server.data.dto.AeropuertoAssembler;
@@ -13,11 +10,6 @@ import es.deusto.ingenieria.sd.easyB.server.data.dto.AeropuertoDTO;
 import es.deusto.ingenieria.sd.easyB.server.data.dto.VueloAssembler;
 import es.deusto.ingenieria.sd.easyB.server.data.dto.VueloDTO;
 import es.deusto.ingenieria.sd.easyB.server.gateway.FactGatewayAerolinea;
-<<<<<<< HEAD
-import es.deusto.ingenieria.sd.easyB.server.gateway.IGatewayAerolinea;
-=======
-import es.deusto.ingenieria.sd.easyB.server.gateway.FactGatewayAerolinea.TipoAerolineas;
->>>>>>> branch 'master' of https://github.com/AneCapde/EasyBookingServer.git
 
 public class BusquedaVuelosService {
 	
@@ -41,14 +33,10 @@ public class BusquedaVuelosService {
 		return VueloAssembler.getInstance().entityToDTO(DBManager.getInstance().getVuelos(aeropuertoName));
 	}
 	
-<<<<<<< HEAD
-	public List<AerolineaDTO> getAerolineas(String tipoAerolinea) {
-		return AerolineaAssembler.getInstance().entityToDTO(FactGatewayAerolinea.);
-=======
-	public List<AerolineaDTO> getAerolineas() {
-		return AerolineaAssembler.getInstance().entityToDTO(FactGatewayAerolinea.getInstance().createGateway(TipoAerolineas.Vueling));
->>>>>>> branch 'master' of https://github.com/AneCapde/EasyBookingServer.git
+	//Este metodo no tenia claro lo que tiene que obtener
+	@SuppressWarnings("unchecked")
+	public List<AerolineaDTO> getAerolineas(Aerolinea aerolineas) {
+		//return AerolineaAssembler.getInstance().entityToDTO(FactGatewayAerolinea.getInstance().createGateway(TipoAerolineas.Vueling));
+		return (List<AerolineaDTO>) AerolineaAssembler.getInstance().entityToDTO(aerolineas);
 	}
-
-	//Cambiar esto para hacerlo con el DBManager, y la aerolinea cojer el enum del factoryAerolinea
 }
