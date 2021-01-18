93fe0c06fa0bec21580e3e594c3a45d6aa9967a7
olatzinfante <47973617+olatzinfante@users.noreply.github.com>
2021-01-18 23:38:13 +0100

    .jar
    hay métodos que cambiar en en IEasyBookingRemoteFacade

--- EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/remote/IEasyBookingRemoteFacade.java
+++ EasyBookingServer/src/es/deusto/ingenieria/sd/easyB/server/remote/IEasyBookingRemoteFacade.java
@@ -18,7 +18,11 @@ public interface IEasyBookingRemoteFacade extends Remote {
 	public boolean registrarUsuario(String email, String password, String tipoPago, String aeroPref) throws RemoteException;
 	//public ArrayList<AerolineaDTO> getAerolineas() throws RemoteException;
 	public ArrayList<AeropuertoDTO> getAeropuertos() throws RemoteException;
+	
+	//Hay que cambiar este método
 	public ArrayList<VueloDTO> buscarVuelos(Aeropuerto origen, Aeropuerto destino, Date fecha, int num_pasajeros) throws RemoteException;
+	//Hay que cambiar este método
 	public boolean reservaVuelos(Vuelo vuelo, double importe, int num_pasajeros, Date fecha, ArrayList<String> nombre_pasajeros) throws RemoteException;
+	
 	public boolean realizarPago(String email, String password, double cantidad) throws RemoteException;
 }
